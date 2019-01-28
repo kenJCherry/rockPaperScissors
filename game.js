ecb0ecb15091ff535eb9fc88f08fdae17c4d213e
Ken Cherry <kencherry3e851@gamil.com>
2019-01-28 10:58:50 -0500

    Play Game

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
+
 const getUserChoice = userInput => {
     userInput = userInput.toLowerCase();
-    if( userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' ){
+    if( userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb' ){
       return userInput;
     } else {
       console.log( 'Not a proper response, please try again' );
@@ -26,6 +27,10 @@ function getComputerChoice() {
 }//End getComputerChoice
 
 function determineWinner(userChoice, computerChoice){
+  if ( userChoice === 'bomb' ){
+    return 'You won!'
+  }
+
   if ( userChoice === computerChoice ){
     return 'The game is a tie!';
   }
@@ -53,27 +58,35 @@ function determineWinner(userChoice, computerChoice){
         return 'The user won!';
       }
     }
-}
-//End determinWinner
+}//End determinWinner
+
+
+const playGame = () => {
+  const userChoice = getUserChoice('rock');
+  const computerChoice = getComputerChoice();
+  console.log(`You threw: ${userChoice}`);
+  console.log(`The computer threw: ${computerChoice}`);
+  console.log(determineWinner(userChoice, computerChoice));
+  };
+
+playGame();
+
 
 //console.log(getComputerChoice());
 //console.log(getUserChoice('paper'));
-console.log(determineWinner('paper', 'scissors'))
-console.log(determineWinner('paper', 'paper'))
-console.log(determineWinner('paper', 'rock'))
+//console.log(determineWinner('paper', 'scissors'));
+//console.log(determineWinner('paper', 'paper'));
+//console.log(determineWinner('paper', 'rock'));
 
-/*
+/*Test secret cheat code
+console.log(determineWinner('bomb', 'rock'));
+console.log(determineWinner('bomb', 'paper'));
+console.log(determineWinner('bomb', 'scissors'));
+*/
+
+/*Test getUserChoice
 console.log(getUserChoice('rock'));
 console.log(getUserChoice('scissors'));
 console.log(getUserChoice('knife'));
+console.log(getUserChoice('bomb'));
 */
-
- /*
-  if( userChoice === computerChoice ){
-    return 'The game is a tie!';
-  } else if( userChoice === 'rock' && computerChoice === 'paper'){
-    	return 'The computer won!';
-    } else{
-      return 'The user won!';
-    }
-    */
